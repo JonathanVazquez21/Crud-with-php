fb879e3c0a8ddf9561665cf7aab5042209ea603e
Jonathan Vazquez <jonavaz831@gmail.com>
2021-09-14 23:01:49 -0500

    Crud finish

---
+++ db.php
@@ -0,0 +1,16 @@
+<?php
+
+session_start();
+
+$conn = mysqli_connect(
+    'localhost',
+    'root',
+    '',
+    'phpcrud'
+);
+
+// if (isset($conn)) {
+//     echo 'DB is connected';
+// }
+
+?>
